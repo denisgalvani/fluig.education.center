b9b9a346cc7f6440cc1f285317e79828d10c24d1
Denis Galvani <denisgalvani@users.noreply.github.com>
2016-09-01 16:31:09 -0300

    Create validateProcessAttachments.js
    Processos - Validação de anexos incluídos na solicitação workflow
    
    Link origem: http://fluig.totvs.com/portal/p/10097/post/fluig-dev-partner/4599139

---
+++ BPM/Exemplos/Eventos/validateProcessAttachments.js
@@ -0,0 +1,22 @@
+var anexos = hAPI.listAttachments();
+
+//if (anexos.empty){
+//  throw "Favor anexar";
+//}
+
+var hasAttachment = false;
+for (var i = 0; i < anexos.size(); i++) {
+
+  var doc = anexos.get(i);
+  log.info(doc.getDocumentDescription());
+
+  if (doc.getDocumentDescription() == "CONTRATO_ASSINADO.pdf") {
+    hasAttachment = true;
+    log.info("<<< CONTRATO INCLUIDO COM SUCESSO");
+  }
+
+}
+
+if (!hasAttachment) {
+  log.info("<<< NAO ENCONTROU - CONTRATO_ASSINADO.pdf");
+}
